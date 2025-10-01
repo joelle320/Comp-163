066e976d089e5b6f1114a5435a067a7bea13343a
joelle320 <ljroberson@aggies.ncat.edu>
2025-10-01 15:10:57 -0400

    pushing up in class code from Sep-29

---
+++ In-Class/sep-29.py
@@ -0,0 +1,9 @@
+item = 0
+while item in range(5):
+    item += 1
+    print(item)
+print("-------------------------------")
+for item in range(10):
+    if item % 2 == 0:
+        item += 2
+        print(item) 
